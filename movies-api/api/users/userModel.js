40bec9cae9604ec5098a0f3913a6d040f9a60b56
joc1717 <144799822+joc1717@users.noreply.github.com>
2023-12-18 22:41:10 +0000

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -7,11 +8,36 @@ const UserSchema = new Schema({
   password: {type: String, required: true }
 });
 
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
+
 const passwordValidator = (password) => {
   const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
   return regex.test(password);
 };
 
 UserSchema.path("password").validate(passwordValidator);
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, tihs.password); 
+};
 
 export default mongoose.model('User', UserSchema);
